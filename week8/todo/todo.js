bb8901ec0e6ce1cd99cd1fe0df7b79579b34aa20
shanacaroti <shana.caroti@students.fhnw.ch>
2022-05-04 07:53:53 +0200

    started with todo app

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -1,13 +1,30 @@
+const item = document.getElementById("todoItem");
+const numOfTask = document.getElementById("numberOfTasks");
+const openTasks = document.getElementById("openTasks");
+let taskCounter;
+let openTaskCounter;
 
 function startTodo() {
-
+ taskCounter = 0;
+ openTaskCounter = 0;
+ item.value = "";
 }
 
 function addTodo() {
+
     const container = document.getElementById("todoContainer");
     container.innerHTML += `
     <tr>
-        <td><input type="text"></td>
+        <td><input type="text" value="${item.value}"></td>
     </tr>`;
+
+    // increment number of tasks and open tasks
+    taskCounter++;
+    openTaskCounter++;
+    numOfTask.innerHTML = taskCounter;
+    openTasks.innerHTML = openTaskCounter;
+
+    // clear input field
+    item.value = "";
 
 }
